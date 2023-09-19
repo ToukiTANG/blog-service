dd7a2c83afd8376e096ca6c15313660145056338
Touki <Touki.tangqi@qq.com>
2023-09-19 21:51:50 +0800

    重写登录逻辑

--- src/main/java/com/touki/blog/filter/LoginFilter.java
+++ src/main/java/com/touki/blog/filter/LoginFilter.java
@@ -2,14 +2,18 @@ package com.touki.blog.filter;
 
 import com.touki.blog.constant.RespCode;
 import com.touki.blog.model.dto.AuthUser;
+import com.touki.blog.model.entity.SysUser;
 import com.touki.blog.model.vo.LoginVo;
 import com.touki.blog.model.vo.Result;
 import com.touki.blog.model.vo.UserVo;
+import com.touki.blog.util.JsonUtil;
 import com.touki.blog.util.JwtUtil;
 import com.touki.blog.util.NetworkUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.security.authentication.AccountStatusException;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -23,6 +27,25 @@ import java.io.IOException;
  * @author Touki
  */
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        if (!"POST".equals(request.getMethod())) {
+            throw new AuthenticationServiceException("Authentication method not supported: " + request.getMethod());
+        }
+        try {
+            SysUser user = JsonUtil.readValue(request.getInputStream(), SysUser.class);
+            if (user != null) {
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
+                setDetails(request, authenticationToken);
+                return getAuthenticationManager().authenticate(authenticationToken);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
                                             FilterChain chain, Authentication authResult) throws IOException {
